82f2ad6826c84d8046798d110ab4310fba8a83e2
Rohan Hundia <rh208@st-andrews.ac.uk>
2018-08-04 16:15:28 -0400

    added

---
+++ exploration_GoodSubjects.m
@@ -0,0 +1,9 @@
+function goodOrder= exploration_GoodSubjects()
+% Returns which subjects are good to analyze, 
+% as indices in the subjects array returned by contextGetSubjectsDirsAndRuns
+% which includes all subjects in the order in which they performed the
+% experiment.
+
+goodOrder = [1 2 3 4 5 6 7];
+
+end
