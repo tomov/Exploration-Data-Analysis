55c1b4ecc4a3e6057b3367c6ffa2caffd828a08e
Momchil Tomov <tomov90@gmail.com>
2018-08-07 21:02:48 -0400

    readme ncfvdi

--- exploration_expt.m
+++ exploration_expt.m
@@ -23,7 +23,7 @@ function EXPT = exploration_expt(local)
     %
     if nargin < 1
         [~, name] = system('hostname');
-        if ~isempty(strfind(name, 'momchil'))
+        if ~isempty(strfind(name, 'omchil'))
             % err on the side of falsely thinking it's NCF. Because locally
             % you will catch that mistake immediatley. On NCF, you will
             % catch it after you're already sent 100 jobs and they all
