4400b5cfb60f1e9016fd679e385e543485d19d20
Momchil Tomov <tomov90@gmail.com>
2018-08-15 18:16:18 -0400

    scp_from_ncf

---
+++ include.m
@@ -0,0 +1,21 @@
+% some libraries to include
+%
+
+addpath('/Applications/spm12/'); % SPM itself
+addpath('/Users/momchil/Dropbox/Research/libs/ccnl-fmri/') % sam's ccnl fmri wrappers for SPM
+addpath('/Users/momchil/Dropbox/Research/libs/mfit/'); % sam's mfit library; uses fconmin
+addpath('/Users/momchil/Dropbox/Research/libs/bspmview/'); % for viewing fMRI data
+addpath('/Users/momchil/Dropbox/Research/libs/marsbar-0.44/'); % for ROI analysis
+addpath('/Users/momchil/Dropbox/Research/libs/aal/'); % Automated anatomical labeling
+addpath('/Users/momchil/Dropbox/Research/libs/NIfTI_20140122/'); % Nifti file manipulation
+addpath('/Users/momchil/Dropbox/Research/libs/xml2struct/'); % reading XML files
+addpath('/Users/momchil/Dropbox/Research/libs/glmnet_matlab/'); % generalized linear model fitting toolbox
+addpath('/Users/momchil/Dropbox/Research/libs/utils/'); % useful one-off utils from MathWorks
+addpath('/Users/momchil/Dropbox/Research/libs/rsatoolbox/Engines/'); % RSA toolbox
+addpath('/Users/momchil/Dropbox/Research/libs/rsatoolbox/Modules/'); % RSA toolbox
+addpath('/Users/momchil/Dropbox/Research/libs/distributionPlot/'); % for plotting stuff
+addpath('/Users/momchil/Dropbox/Research/libs/panel-2.12/'); % tight subplots
+addpath('/Users/momchil/Dropbox/Research/libs/panel-2.12/demo/'); % tight subplots
+
+
+%addpath('/Users/momchil/Dropbox/Research/context/code'); % the current directory -- b/c ccnl_fmri_glm.m calls functions in it from another directory
---
+++ scp_from_ncf.sh
@@ -0,0 +1,28 @@
+# Copy group-level contrasts for given model(s) from cluster to local directory.
+#
+# Edit the for loop (loops over models) and change the password where it says password.
+# Creates a directory glmOutput in the current directory and copies stuff there.
+# Make sure to install sshpass first (see below).
+# WARNING: Make sure NOT to upload this to github after putting in an actual password
+#
+
+# STEP 1: install sshpass
+# do it the hard way -- download from http://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz
+# then ./configure
+# then sudo make install
+# which sshpass <-- to find out where it is, and use that path in the --rsh thing below
+
+
+rsync -avh --rsh="/usr/local/bin/sshpass -p password ssh -o StrictHostKeyChecking=no -l mtomov13" mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/Exploration/glmOutput/mean.nii glmOutput/mean.nii
+for i in {12..13}
+do
+    rsync -avh --rsh="/usr/local/bin/sshpass -p password -o StrictHostKeyChecking=no -l mtomov13" mtomov13@ncflogin.rc.fas.harvard.edu:/ncf/gershman/Lab/Exploration/glmOutput/model$i/con* glmOutput/model$i/
+
+# Optionally copy the subject betas too (WARNING: takes up lots of memory; I recommend using an external HDD
+#
+#    for j in {1..31}
+#    do
+#        rsync -avh --rsh="/usr/local/bin/sshpass -p password ssh -o StrictHostKeyChecking=no -l mtomov13" mtomov13@ncflogin.rc.fas.harvard.edu:/net/rcss11/srv/export/ncf_gershman/share_root/Lab/ConLearn/glmOutput/model$i/subj$j/SPM.mat glmOutput/model$i/subj$j/
+#   #     rsync -avh --rsh="/usr/local/bin/sshpass -p password ssh -o StrictHostKeyChecking=no -l mtomov13" mtomov13@ncflogin.rc.fas.harvard.edu:/net/rcss11/srv/export/ncf_gershman/share_root/Lab/ConLearn/glmOutput/model$i/subj$j/beta* glmOutput/model$i/subj$j/
+#    done
+done
