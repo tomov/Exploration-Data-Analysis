ceccb40b0dbb5a984818e99c77c2f171bdd22ae5
Momchil Tomov <tomov90@gmail.com>
2018-08-31 19:46:14 -0400

    params_*.mat updated

--- residuals_analysis.sh
+++ residuals_analysis.sh
@@ -1,31 +1,31 @@
 mkdir output
 
-outfileprefix="output/residual_analysis"
+outfileprefix="output/residuals_analysis"
 echo File prefix = $outfileprefix
 
 echo ---------------- >> jobs.txt
-echo --- Running residual_analysis  >> jobs.txt
+echo --- Running residuals_analysis  >> jobs.txt
 echo ---------------- >> jobs.txt
 
 # send the job to NCF
 #
-sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residual_analysis(10, \'RU\', \'RU\');exit'"`
+sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residuals_analysis(10, \'RU\', \'RU\');exit'"`
 sbatch_output_split=($sbatch_output)
 job_id=${sbatch_output_split[3]}
-echo residual_analysis.sh 10 RU: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
+echo residuals_analysis.sh 10 RU: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
 
 # send the job to NCF
 #
-sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residual_analysis(19, \'RU\', \'RU\');exit'"`
+sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residuals_analysis(19, \'RU\', \'RU\');exit'"`
 sbatch_output_split=($sbatch_output)
 job_id=${sbatch_output_split[3]}
-echo residual_analysis.sh 19 RU: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
+echo residuals_analysis.sh 19 RU: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
 
 # send the job to NCF
 #
-sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residual_analysis(19, \'TU\', \'TU - trial\');exit'"`
+sbatch_output=`sbatch -p ncf --mem 50000 -t 20-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="matlab -nodisplay -nosplash -nojvm -r $'residuals_analysis(19, \'TU\', \'TU - trial\');exit'"`
 sbatch_output_split=($sbatch_output)
 job_id=${sbatch_output_split[3]}
-echo residual_analysis.sh 19 TU - trial: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
+echo residuals_analysis.sh 19 TU - trial: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
 
 sleep 1
